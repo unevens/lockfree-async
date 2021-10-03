e0eeeaff8a70c756cec044663103ee839de705cb
Dario Mambro <unevens@users.noreply.github.com>
2021-10-03 22:50:12 +0200

    added Async class

--- README.md
+++ README.md
@@ -6,18 +6,19 @@ inter-thread communication.
 The fundamental building block of this library is the "IMB Freelist" multiple-producer multiple-consumer LIFO stack, as
 implemented by Ross Bencina in [Queue World](https://github.com/RossBencina/QueueWorld).
 
-## LockFreeMessenger.hpp
+## Messenger.hpp
 
-The template class `LockFreeMessenger<T>` is a wrapper around the Queue World's multiple-producer multiple-consumer LIFO
-stack. It implements functionality to send and receive data of type `T` between threads in a lock-free way, and to
-preallocate the resources to do so.
+The template class `Messenger<T>` is a wrapper around the Queue World's multiple-producer multiple-consumer LIFO stack.
+It implements functionality to send and receive data of type `T` between threads in a lock-free way, and to preallocate
+the resources to do so.
 
 ## PreAllocated.hpp
 
 The template class `PreAllocated<T>` owns an object of class `T` which can be shared between a non realtime thread and a
-realtime thread, so that the non realtime thread can perform any blocking operation, such as creating the object, and the realtime thread can use the
-object.
+realtime thread, so that the non realtime thread can perform any blocking operation, such as creating the object, and
+the realtime thread can use the object.
 
-## Development
+## Async.hpp
 
-There is more on the dev branch, but it is a work in progress to which the author is not particularly committed.
+The template class `Async` manages asynchronous creation and destruction of several instances of an object, that can be
+requested and used from realtime threads, while being created, modified and destroyed by a timer thread.
---
+++ lockfree/Async.hpp
@@ -0,0 +1,295 @@
+/*
+Copyright 2021 Dario Mambro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+#include "Messenger.hpp"
+#include "inplace_function.h"
+#include <chrono>
+#include <mutex>
+#include <thread>
+#include <unordered_set>
+#include <utility>
+
+namespace lockfree {
+
+class AsyncInterface
+{
+  friend class AsyncThread;
+
+public:
+  virtual ~AsyncInterface() = default;
+
+protected:
+  virtual void timerCallback() = 0;
+  class AsyncThread* asyncThread{ nullptr };
+  void setAsyncThread(AsyncThread* asyncThread_) { asyncThread = asyncThread_; }
+  std::mutex& getMutex();
+};
+
+class AsyncThread final
+{
+  friend AsyncInterface;
+
+public:
+  explicit AsyncThread(int timerPeriod = 250)
+    : timerPeriod{ timerPeriod }
+  {}
+
+  void addObject(AsyncInterface& async)
+  {
+    auto const lock = std::lock_guard<std::mutex>(mutex);
+    asyncObjects.insert(&async);
+    async.setAsyncThread(this);
+  }
+
+  void removeObject(AsyncInterface& async)
+  {
+    auto const lock = std::lock_guard<std::mutex>(mutex);
+    asyncObjects.erase(&async);
+    async.setAsyncThread(nullptr);
+  }
+
+  void start()
+  {
+    auto const lock = std::lock_guard<std::mutex>(mutex);
+    if (isRunningFlag) {
+      return;
+    }
+    stopTimerFlag = false;
+    timer = std::thread([this]() {
+      while (true) {
+        for (auto asyncObject : asyncObjects)
+          asyncObject->timerCallback();
+        if (stopTimerFlag) {
+          return;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(timerPeriod));
+        if (stopTimerFlag) {
+          return;
+        }
+      }
+    });
+  }
+
+  void stop()
+  {
+    auto const lock = std::lock_guard<std::mutex>(mutex);
+    stopTimerFlag = true;
+    if (timer.joinable()) {
+      timer.join();
+    }
+    isRunningFlag = false;
+  }
+
+  /**
+   * Sets the period of the timer thread that handles the messages.
+   */
+  void setTimerPeriod(int period)
+  {
+    auto const lock = std::lock_guard<std::mutex>(mutex);
+    timerPeriod.store(period, std::memory_order_release);
+  }
+
+  /**
+   * @return true if the timer thread is running, false otherwise.
+   */
+  bool isRunning() const
+  {
+    return isRunningFlag.load(std::memory_order_acquire);
+  }
+
+  int getTimerPeriod() const
+  {
+    return timerPeriod.load(std::memory_order_acquire);
+  }
+
+  ~AsyncThread()
+  {
+    stopTimer();
+    for (auto asyncObject : asyncObjects)
+      removeObject(*asyncObject);
+  }
+
+private:
+  std::mutex& getMutex() { return mutex; }
+  std::unordered_set<AsyncInterface*> asyncObjects;
+  std::thread timer;
+  std::atomic<bool> stopTimerFlag{ false };
+  std::atomic<int> timerPeriod;
+  std::atomic<bool> isRunningFlag{ false };
+  std::mutex mutex;
+};
+
+inline std::mutex&
+AsyncInterface::getMutex()
+{
+  return asyncThread->mutex;
+}
+
+template<class TObject,
+         class TObjectSettings,
+         size_t ChangeFunctorClosureCapacity = 32>
+class Async final : public AsyncInterface
+{
+public:
+  using ObjectSettings = TObjectSettings;
+  using Object = TObject;
+  using ChangeSettings = stdext::inplace_function<void(ObjectSettings&),
+                                                  ChangeFunctorClosureCapacity>;
+
+private:
+  struct Instance final
+  {
+    std::unique_ptr<Object> object;
+    Messenger<Object> toInstance;
+    Messenger<Object> fromInstance;
+
+    explicit Instance(ObjectSettings& objectSettings)
+      : object{ objectSettings }
+    {}
+  };
+
+public:
+  class InstanceAccess final
+  {
+    template<class TObject_, class TObjectSettings_>
+    friend class Async;
+
+  public:
+    bool update()
+    {
+      using std::swap;
+      auto messageNode = instance->toInstance.receiveLastNode();
+      if (messageNode) {
+        auto& object = messageNode->get();
+        swap(instance->object, object);
+        instance->fromInstance.send(messageNode);
+        return true;
+      }
+      return false;
+    }
+
+    Object& get() { return instance->object; }
+
+    Object const& get() const { return instance->object; }
+
+    ~InstanceAccess()
+    {
+      if (asyncThread) {
+        auto const lock = std::lock_guard<std::mutex>(asyncThread->getMutex());
+        async.removeInstance(instance);
+      }
+      else {
+        async.removeInstance(instance);
+      }
+    }
+
+  private:
+    explicit InstanceAccess(Async& async, Instance* instance)
+      : async{ async }
+      , instance{ instance }
+    {}
+
+    Async& async;
+    Instance* instance;
+  };
+
+  friend InstanceAccess;
+
+  std::unique_ptr<InstanceAccess> requestInstance()
+  {
+    if (asyncThread) {
+      auto const lock = std::lock_guard<std::mutex>(asyncThread->getMutex());
+      return createInstance();
+    }
+    else {
+      return createInstance();
+    }
+  }
+
+  bool submitChange(ChangeSettings&& change) { return messenger.send(change); }
+
+  bool submitChangeIfNodeAvailable(ChangeSettings&& change)
+  {
+    return messenger.sendIfNodeAvailable(change);
+  }
+
+  void preallocateNodes(int numNodesToPreallocate)
+  {
+    messenger.preallocateNodes(numNodesToPreallocate);
+  }
+
+  ~Async()
+  {
+    if (asyncThread) {
+      asyncThread->removeObject(*this);
+    }
+  }
+
+private:
+  std::unique_ptr<InstanceAccess> createInstance()
+  {
+    instances.push_back(std::make_unique<Instance>(objectSettings));
+    return std::make_unique<InstanceAccess>(*this, instances.back().get());
+  }
+
+  void removeInstance(Instance* instance)
+  {
+    auto it =
+      std::find_if(instances.begin(), instances.end(), [](auto& instance) {
+        return instace.get() == instanceAccess.instanceAddress;
+      });
+    assert(it != instances.end());
+    if (it == instances.end())
+      return;
+    instances.erase(it);
+  }
+
+  bool handleChanges()
+  {
+    int numChanges = receiveAndHandleMessageStack(
+      messenger, [&](ChangeSettings& change) { change(objectSettings); });
+    return numChanges > 0;
+  }
+
+  void timerCallback() override
+  {
+    auto const numObjects = objects.size();
+    for (auto& instance : instances) {
+      freeMessageStack(instance->fromInstance.receiveAllNodes());
+    }
+    bool const anyChange = handleChanges();
+    if (anyChange) {
+      for (auto& instance : instances) {
+        freeMessageStack(instance->toInstance.receiveAllNodes());
+        instance->toInstance.send(Object{ objectSettings });
+      }
+    }
+  }
+
+  AsyncThread* asyncThread{ nullptr };
+  std::vector<std::unique_ptr<Instance>> instances;
+  Messenger<ChangeSettings> messenger;
+  ObjectSettings objectSettings;
+};
+
+} // namespace lockfree
