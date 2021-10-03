f3e652be131b879e34dcadcb3f3aa69dd20b5970
Dario Mambro <unevens@users.noreply.github.com>
2021-10-03 16:40:46 +0200

    added PreAllocated

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # lockfree-async
 
-[lockfree-async](https://github.com/unevens/lockfree-async) is a C++ header-only simple template library for lock-free inter-thread communication.
+[lockfree-async](https://github.com/unevens/lockfree-async) is a C++ header-only simple template library for lock-free
+inter-thread communication.
 
-The fundamental building block of this library is the "IMB Freelist" multiple-producer multiple-consumer LIFO stack, as implemented by Ross Bencina in [Queue World](https://github.com/RossBencina/QueueWorld).
+The fundamental building block of this library is the "IMB Freelist" multiple-producer multiple-consumer LIFO stack, as
+implemented by Ross Bencina in [Queue World](https://github.com/RossBencina/QueueWorld).
 
 ## LockFreeMessenger.hpp
 
-The template class `LockFreeMessenger<T>` is a wrapper around the Queue World's multiple-producer multiple-consumer LIFO stack. It implements functionality to send and receive data of type `T` between threads in a lock-free way, and to preallocate the resources to do so. 
+The template class `LockFreeMessenger<T>` is a wrapper around the Queue World's multiple-producer multiple-consumer LIFO
+stack. It implements functionality to send and receive data of type `T` between threads in a lock-free way, and to
+preallocate the resources to do so.
+
+## PreAllocated.hpp
+
+The template class `PreAllocated<T>` owns an object of class `T` which can be shared between a non realtime thread and a
+realtime thread, so that the non realtime thread can perform any blocking operation, such as creating the object, and the realtime thread can use the
+object.
 
 ## Development
 
---
+++ lockfree/PreAllocated.hpp
@@ -0,0 +1,72 @@
+/*
+Copyright 2021 Dario Mambro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#pragma once
+#include "LockFreeMessenger.hpp"
+#include <cassert>
+
+namespace lockfree {
+
+template<class Object>
+class PreAllocated final
+{
+public:
+  Object* getFromRealtimeThread()
+  {
+    auto newObject = messengerForNewObjects.receiveLastMessage();
+    if (newObject) {
+      messengerForOldObjects.send(std::move(currentObjectStorage));
+      currentObjectStorage = std::move(newObject.value());
+      currentObjectPtr.store(currentObjectStorage.get(), std::memory_order_release);
+    }
+    return currentObjectStorage.get();
+  }
+
+  Object* getFromNonRealtimeThread()
+  {
+    return currentObjectPtr.load(std::memory_order_acquire);
+  }
+
+  void set(std::unique_ptr<Object> newObject)
+  {
+    lockfree::receiveAndHandleMessageStack(messengerForOldObjects, [](auto& object) { object.reset(); });
+    messengerForNewObjects.send(std::move(newObject));
+  }
+
+  void preallocateMessageNodes(int numNodesToPreallocate)
+  {
+    messengerForNewObjects.preallocateNodes(numNodesToPreallocate);
+    messengerForOldObjects.preallocateNodes(numNodesToPreallocate);
+  }
+
+  explicit PreAllocated(int numNodesToPreallocate = 128)
+  {
+    preallocateMessageNodes(numNodesToPreallocate);
+  }
+
+private:
+  lockfree::Messenger<std::unique_ptr<Object>> messengerForNewObjects;
+  lockfree::Messenger<std::unique_ptr<Object>> messengerForOldObjects;
+  std::unique_ptr<Object> currentObjectStorage;
+  std::atomic<Object*> currentObjectPtr{ nullptr };
+};
+
+} // namespace unplug
